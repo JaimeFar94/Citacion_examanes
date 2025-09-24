c3f2eecc18e535016fd1a1601d2c030d31a10d99
JaimeFar94 <157666753+JaimeFar94@users.noreply.github.com>
2025-09-24 14:31:14 -0500

    cambios en Build

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ set -o errexit
 pip install -r requirements.txt
 
 #Inicializar las migraciones si no existen
-python manage.py collectstatic --no --input
+python manage.py collectstatic --no-input
 python manage.py migrate
